b5e271a515764a324aedbcdb1af927cc2fadf0bf
BITTU KUMAR AZAD <bittukrazad652@gmail.com>
2025-11-25 20:28:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-GA_ID = "G-S4K4NGKXLH"
-
-components.html(
-    f"""
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){{dataLayer.push(arguments);}}
-      gtag('js', new Date());
-      gtag('config', '{GA_ID}');
-    </script>
-    """,
-    height=0,  # keeps it invisible
-)
 
 # Pre-download model during build
 import os
